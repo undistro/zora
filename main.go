15a8c0880d54b3d43f2fc2b3ddc85ec7c3c5ac1f
Matheus Moraes <matheusfaria.moraes@gmail.com>
2022-04-11 21:49:27 -0300

    generating cluster resource and controller

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	snitchundistroiov1alpha1 "github.com/getupio-undistro/snitch/api/v1alpha1"
+	"github.com/getupio-undistro/snitch/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -25,6 +28,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(snitchundistroiov1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -58,6 +62,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ClusterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
