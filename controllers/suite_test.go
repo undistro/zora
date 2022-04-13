b4cac705803f7c1a4aff90ca425c26fa1ec83bf3
Matheus Moraes <matheusfaria.moraes@gmail.com>
2022-04-13 09:43:24 -0300

    renaming alias of api package import, and setting RFC3339 as time encoder on logs (#2)

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -14,7 +14,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	snitchundistroiov1alpha1 "github.com/getupio-undistro/snitch/api/v1alpha1"
+	"github.com/getupio-undistro/snitch/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -46,7 +46,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = snitchundistroiov1alpha1.AddToScheme(scheme.Scheme)
+	err = v1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
