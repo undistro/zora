643939347dddc90cd4c1f18d0e8411d5bb0dcf4f
Matheus Moraes <matheusfaria.moraes@gmail.com>
2022-05-03 10:17:57 -0300

    logging parameters in helm chart (#26)

--- controllers/cluster_controller.go
+++ controllers/cluster_controller.go
@@ -33,7 +33,7 @@ type ClusterReconciler struct {
 //+kubebuilder:rbac:groups=inspect.undistro.io,resources=clusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=inspect.undistro.io,resources=clusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=inspect.undistro.io,resources=clusters/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
