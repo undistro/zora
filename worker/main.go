4a700ea72533c6ded8f0b6c75682ef8f231a38db
Carlos Torres <carlos.torres@getupcloud.com>
2022-05-23 09:54:56 -0300

    Add worker's main execution point
    Changes:
    	- Create <run> package with worker's main execution flow;
    	- Add logging to <main.go>;
    
    The <run> package also handles the creation of <ClusterIssue> instances
    on a Kubernetes cluster.
    
    Signed-off-by: Carlos Torres <carlos.torres@getupcloud.com>

---
+++ worker/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	"github.com/getupio-undistro/inspect/worker/run"
+	"go.uber.org/zap/zapcore"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+var log = ctrl.Log.WithName("worker")
+
+func main() {
+	opts := zap.Options{
+		Development: true,
+		TimeEncoder: zapcore.TimeEncoderOfLayout(time.RFC3339),
+	}
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	log.Info("Starting worker")
+	if err := run.Run(); err != nil {
+		log.Info("Worker crashed")
+		panic(err)
+	}
+	log.Info("Worker finished successfully")
+	log.Info("Stopping worker")
+}
