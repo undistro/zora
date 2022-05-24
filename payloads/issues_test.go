c9bcf848909ead772f5a7fe015a2b4f0e26eaf18
Matheus Moraes <matheusfaria.moraes@gmail.com>
2022-05-24 17:22:08 -0300

    sorting issues in unit test

--- payloads/issues_test.go
+++ payloads/issues_test.go
@@ -163,6 +163,9 @@ func TestNewIssues(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := NewIssues(tt.args.issues)
+			sort.Slice(got, func(i, j int) bool {
+				return got[i].ID < got[j].ID
+			})
 			for _, issue := range got {
 				sort.Slice(issue.Clusters, func(i, j int) bool {
 					return issue.Clusters[i].Name < issue.Clusters[j].Name
