ec34f152f19cef2c0856f262ec5a7c811ba877b7
Carlos Torres <carlos.torres@getupcloud.com>
2022-05-25 17:42:06 -0300

    Generalize specific issue messages
    Changes:
    	- Map Popeye issue ids to generic versions of their respective issue
    	  description;
    	- Handle which issue message to use at the id-message splitting
    	  function;
    	- Rename the <splitCodeAndMsg> function as <prepareIdAndMsg> to
    	  reflect the new behavior;
    	- Move <LevelToIssueSeverity> map to a new file, together with the
    	  id-message map;
    
    Signed-off-by: Carlos Torres <carlos.torres@getupcloud.com>

---
+++ worker/report/popeye/parse_types.go
@@ -0,0 +1,85 @@
+package popeye
+
+import inspectv1a1 "github.com/getupio-undistro/inspect/apis/inspect/v1alpha1"
+
+var (
+	// LevelToIssueSeverity maps Popeye's <Level> type to Inspect's
+	// <ClusterIssueSeverity>.
+	LevelToIssueSeverity = [4]inspectv1a1.ClusterIssueSeverity{
+		inspectv1a1.SeverityNone,
+		inspectv1a1.SeverityLow,
+		inspectv1a1.SeverityMedium,
+		inspectv1a1.SeverityHigh,
+	}
+
+	// IssueIDtoGenericMsg maps Popeye's issue codes to generic versions of the
+	// issue description. The original issues can be found on Popeye's source
+	// file <internal/issues/assets/codes.yml>.
+	IssueIDtoGenericMsg = map[string]string{
+		// Container
+		"105": "Unnamed probe port in use",
+		"108": "Unnamed port",
+		"109": "CPU request reached user's request threashold",
+		"110": "Memory request reached user's request threashold",
+		"111": "CPU reached user's limit threashold",
+		"112": "Memory reached user's limit threashold",
+		"113": "Container image not hosted on an allowed docker registry",
+
+		// Pod
+		"200": "Pod is terminating",
+		"201": "Pod is terminating a process",
+		"202": "Pod is waiting",
+		"203": "Pod is waiting a process",
+		"204": "Pod is not ready",
+		"205": "Pod was restarted",
+		"207": "Pod is in an unhappy phase",
+
+		// Security
+		"304": "References a secret which does not exist",
+		"305": "References a docker-image pull secret which does not exist",
+
+		// General
+		"401": "Unable to locate key reference",
+		"402": "No metric-server detected",
+		"403": "Deprecated API group",
+		"404": "Deprecation check failed",
+
+		// Deployment and StatefulSet
+		"501": "Unhealthy, mismatch between desired and available state",
+		"503": "At current load, CPU under allocated",
+		"504": "At current load, CPU over allocated",
+		"505": "At current load, Memory under allocated",
+		"506": "At current load, Memory over allocated",
+		"507": "Deployment references ServiceAccount which does not exist",
+
+		// HPA
+		"600": "HPA references a Deployment which does not exist",
+		"601": "HPA references a StatefulSet which does not exist",
+		"602": "Replicas at burst will match or exceed cluster CPU capacity",
+		"603": "Replicas at burst will match or exceed cluster memory capacity",
+		"604": "If ALL HPAs are triggered, cluster CPU capacity will match or exceed threshold",
+		"605": "If ALL HPAs are triggered, cluster memory capacity will match or exceed threshold",
+
+		// Node
+		"700": "Found taint that no pod can tolerate",
+		"709": "CPU threshold reached",
+		"710": "Memory threshold reached",
+
+		// PodDisruptionBudget
+		"901": "MinAvailable is greater than the number of pods currently running",
+
+		// Service
+		"1102": "Unnamed service port in use",
+		"1106": "No target ports match service port",
+
+		// ReplicaSet
+		"1120": "Unhealthy ReplicaSet",
+
+		// NetworkPolicies
+		"1200": "No pods match pod selector",
+		"1201": "No namespaces match namespace selector",
+
+		// RBAC
+		"1300": "Inexistent reference",
+	}
+)
