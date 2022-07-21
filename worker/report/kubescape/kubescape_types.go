c2ed374b885da886b5502a7164c43a27ae1ab05b
Carlos Torres <carlos.torres@getupcloud.com>
2022-07-21 16:55:18 -0300

    Clean Kubescape report type
    Remove remaining unused fields from the types used to represent a
    Kubescape report.
    
    Signed-off-by: Carlos Torres <carlos.torres@getupcloud.com>

--- worker/report/kubescape/kubescape_types.go
+++ worker/report/kubescape/kubescape_types.go
@@ -16,66 +16,27 @@ const (
 	StatusError      ScanningStatus = "error"
 )
 
-type StatusInfo struct {
-	InnerStatus ScanningStatus `json:"status,omitempty"`
-	InnerInfo   string         `json:"info,omitempty"`
-}
-
-// Adapted from Armosec's package
-// <github.com/armosec/opa-utils/reporthandling/helpers/v1>
-
-// ResourceCounters stores the total amount of Kubernetes resources which,
-// passed, failed or where excluded from the scan.
-type ResourceCounters struct {
-	PassedResources   int `json:"passedResources"`
-	FailedResources   int `json:"failedResources"`
-	ExcludedResources int `json:"excludedResources"`
-}
-
 // Adapted from Armosec's package
 // <github.com/armosec/opa-utils/reporthandling/results/v1/reportsummary>
 
-// ControlSummary contains a summary of a scan Control with the score,
-// status, counters, etc.
+// ControlSummary contains the scan Control with the scorefactor.
 type ControlSummary struct {
-	ControlID        string           `json:"controlID"`
-	Name             string           `json:"name"`
-	Status           ScanningStatus   `json:"status"`
-	Score            float32          `json:"score"`
-	ScoreFactor      float32          `json:"scoreFactor"`
-	ResourceCounters ResourceCounters `json:",inline"`
+	ScoreFactor float32 `json:"scoreFactor"`
 }
 
-// FrameworkSummary contains a summary of a scan Framework with the score,
-// versions, counters, etc.
-type FrameworkSummary struct {
-	Name             string                    `json:"name"`
-	Status           ScanningStatus            `json:"status"`
-	Score            float32                   `json:"score"`
-	Version          string                    `json:"version"`
-	Controls         map[string]ControlSummary `json:"controls,omitempty"`
-	ResourceCounters ResourceCounters          `json:",inline"`
-}
-
-// SummaryDetails contains a summary of the scan with the score, versions,
-// counters, etc.
+// SummaryDetails contains a summary of the scan with the status and per
+// Control summary.
 type SummaryDetails struct {
-	Score            float32                   `json:"score"`
-	Status           ScanningStatus            `json:"status"`
-	Frameworks       []FrameworkSummary        `json:"frameworks"`
-	Controls         map[string]ControlSummary `json:"controls,omitempty"`
-	ResourceCounters ResourceCounters          `json:",inline"`
+	Status   ScanningStatus            `json:"status"`
+	Controls map[string]ControlSummary `json:"controls,omitempty"`
 }
 
 // Adapted from Armosec's package
 // <github.com/armosec/opa-utils/reporthandling/results/v1/resourcesresults>
 
-// ResourceAssociatedRule holds a failed rule associated to a Kubernetes
-// resource.
+// ResourceAssociatedRule holds the REGO rule associated status.
 type ResourceAssociatedRule struct {
-	Name                  string              `json:"name"`
-	Status                ScanningStatus      `json:"status"`
-	ControlConfigurations map[string][]string `json:"controlConfigurations,omitempty"` // new
+	Status ScanningStatus `json:"status"`
 }
 
 // ResourceAssociatedControl holds the Control that is associated to a
