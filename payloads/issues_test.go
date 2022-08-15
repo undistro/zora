dcd5c57b3d6d51b7e65a2e5700e23b51191a94e5
Carlos Torres <carlos.torres@getupcloud.com>
2022-08-15 18:24:39 -0300

    Adapt <payloads.issues> tests
    Changes:
    	- Adapt the existing payloads unit test to reflect the changes on
    	  the <NewIssues> function;
    	- Add a test case for issues from clusters with scan failure;
    
    The only modification to the pre-exiting test case was the addition of a
    parameter required by <NewIssues>.
    
    Signed-off-by: Carlos Torres <carlos.torres@getupcloud.com>

--- payloads/issues_test.go
+++ payloads/issues_test.go
@@ -12,7 +12,8 @@ import (
 
 func TestNewIssues(t *testing.T) {
 	type args struct {
-		issues []v1alpha1.ClusterIssue
+		failedscans map[string]struct{}
+		issues      []v1alpha1.ClusterIssue
 	}
 	tests := []struct {
 		name string
@@ -22,6 +23,7 @@ func TestNewIssues(t *testing.T) {
 		{
 			name: "OK",
 			args: args{
+				failedscans: map[string]struct{}{},
 				issues: []v1alpha1.ClusterIssue{
 					{
 						ObjectMeta: metav1.ObjectMeta{
@@ -159,10 +161,145 @@ func TestNewIssues(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "Issues from cluster with failed scan",
+			args: args{
+				failedscans: map[string]struct{}{
+					"prd1": {},
+					"dev1": {},
+				},
+				issues: []v1alpha1.ClusterIssue{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "prd1-pop-106-123",
+							Namespace: "prd",
+							Labels: map[string]string{
+								v1alpha1.LabelScanID: "123",
+							},
+						},
+						Spec: v1alpha1.ClusterIssueSpec{
+							Cluster:        "prd1",
+							ID:             "POP-106",
+							Message:        "No resources requests/limits defined",
+							Severity:       "Medium",
+							Category:       "Category",
+							TotalResources: 10,
+						},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "prd1-pop-106-456",
+							Namespace: "prd",
+							Labels: map[string]string{
+								v1alpha1.LabelScanID: "456",
+							},
+						},
+						Spec: v1alpha1.ClusterIssueSpec{
+							Cluster:        "prd1",
+							ID:             "POP-106",
+							Message:        "No resources requests/limits defined",
+							Severity:       "Medium",
+							Category:       "Category",
+							TotalResources: 10,
+						},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "prd2-pop-777-123",
+							Namespace: "prd",
+						},
+						Spec: v1alpha1.ClusterIssueSpec{
+							Cluster:        "prd2",
+							ID:             "POP-777",
+							Message:        "Message",
+							Severity:       "Medium",
+							Category:       "Category",
+							TotalResources: 7,
+						},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "hml1-pop-106-123",
+							Namespace: "hml",
+						},
+						Spec: v1alpha1.ClusterIssueSpec{
+							Cluster:        "hml1",
+							ID:             "POP-106",
+							Message:        "No resources requests/limits defined",
+							Severity:       "Medium",
+							Category:       "Category",
+							TotalResources: 17,
+						},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "dev1-pop-106-123",
+							Namespace: "dev",
+						},
+						Spec: v1alpha1.ClusterIssueSpec{
+							Cluster:        "dev1",
+							ID:             "POP-106",
+							Message:        "No resources requests/limits defined",
+							Severity:       "Medium",
+							Category:       "Category",
+							TotalResources: 71,
+						},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "dev2-pop-777-123",
+							Namespace: "dev",
+						},
+						Spec: v1alpha1.ClusterIssueSpec{
+							Cluster:        "dev2",
+							ID:             "POP-777",
+							Message:        "Message",
+							Severity:       "Medium",
+							Category:       "Category",
+							TotalResources: 27,
+						},
+					},
+				},
+			},
+			want: []Issue{
+				{
+					ID:       "POP-106",
+					Message:  "No resources requests/limits defined",
+					Severity: "Medium",
+					Category: "Category",
+					Clusters: []ClusterReference{
+						{
+							Name:           "hml1",
+							Namespace:      "hml",
+							TotalResources: 17,
+						},
+					},
+				},
+				{
+					ID:       "POP-777",
+					Message:  "Message",
+					Severity: "Medium",
+					Category: "Category",
+					Clusters: []ClusterReference{
+						{
+							Name:           "dev2",
+							Namespace:      "dev",
+							TotalResources: 27,
+						},
+						{
+							Name:           "prd2",
+							Namespace:      "prd",
+							TotalResources: 7,
+						},
+					},
+				},
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewIssues(tt.args.issues)
+			got := NewIssues(tt.args.issues, tt.args.failedscans)
 			sort.Slice(got, func(i, j int) bool {
 				return got[i].ID < got[j].ID
 			})
