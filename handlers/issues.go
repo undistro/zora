8ad3f40a7316d040bc331ef3e89f543a00e659c0
Carlos Torres <carlos.torres@getupcloud.com>
2022-08-15 18:10:04 -0300

    Adapt </issues> endpoint to clusters with scan failure
    Prevent <ClusterIssue> instances pointing to clusters whose scan has
    failed from being listed on the </issues> endpoint. This ensures
    consistency with the cluster endpoint
    </namespaces/<namespace>/clusters/<cluster>>, which already applies this
    filter.
    
    Signed-off-by: Carlos Torres <carlos.torres@getupcloud.com>

--- handlers/issues.go
+++ handlers/issues.go
@@ -7,6 +7,7 @@ import (
 	"github.com/getupio-undistro/zora/payloads"
 	"github.com/getupio-undistro/zora/pkg/clientset/versioned"
 	"github.com/go-logr/logr"
+	batchv1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -21,7 +22,21 @@ func IssueListHandler(client versioned.Interface, logger logr.Logger) func(http.
 			return
 		}
 
-		issues := payloads.NewIssues(issueList.Items)
+		cslist, err := client.ZoraV1alpha1().ClusterScans("").List(r.Context(), metav1.ListOptions{})
+		if err != nil {
+			log.Error(err, "Failed to list ClusterScans")
+			RespondWithDetailedError(w, http.StatusInternalServerError, "Error listing ClusterScans", err.Error())
+			return
+		}
+
+		failedscan := map[string]struct{}{}
+		for _, cs := range cslist.Items {
+			if cs.Status.LastFinishedStatus == string(batchv1.JobFailed) {
+				failedscan[cs.Spec.ClusterRef.Name] = struct{}{}
+			}
+		}
+
+		issues := payloads.NewIssues(issueList.Items, failedscan)
 		log.Info(fmt.Sprintf("%d issue(s) returned", len(issues)))
 		RespondWithJSON(w, http.StatusOK, issues)
 	}
