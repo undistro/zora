8680d3829b19cf3c500cbfe7225d5cf39641d014
Carlos Torres <carlos.torres@getupcloud.com>
2022-08-23 11:10:58 -0300

    Create <ClusterIssueOverride> CRD
    This CRD can be used by sysadmins to override information about each
    cluster issue. It contains 3 fields of same type as its <ClusterIssue>
    counterpart, being them <message>, <severity> and <category>, and
    a field indicating the clusters whereby this override is valid. In case
    no cluster is specified, the override workers globally.
    
    Each <ClusterIssueOverride> instance is named according to a cluster
    issue.
    
    Signed-off-by: Carlos Torres <carlos.torres@getupcloud.com>

---
+++ apis/zora/v1alpha1/clusterissueoverride_types.go
@@ -0,0 +1,53 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ClusterIssueOverrideSpec stores user provided data to superseed the defaults
+// from a given <ClusterIssue>. The last 3 fields carry the same type of
+// information as its <ClusterIssue> equivalent, but the first is a list of
+// clusters where this override is valid. An empty list emplies a global
+// override.
+type ClusterIssueOverrideSpec struct {
+	Clusters []string             `json:"clusters"`
+	Message  string               `json:"message,omitempty"`
+	Severity ClusterIssueSeverity `json:"severity,omitempty"`
+	Category string               `json:"category,omitempty"`
+}
+
+type ClusterIssueOverrideStatus struct{}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:shortName="cio"
+//+kubebuilder:printcolumn:name="Severity",type="string",JSONPath=".spec.severity",priority=0
+//+kubebuilder:printcolumn:name="Category",type="string",JSONPath=".spec.category",priority=0
+//+kubebuilder:printcolumn:name="Message",type="string",JSONPath=".spec.message",priority=0
+//+kubebuilder:printcolumn:name="Clusters",type="string",JSONPath=".spec.clusters",priority=1
+
+// ClusterIssueOverride is the Schema for the clusterissueoverrides API. Its
+// name is always the identifier of a cluster issue.
+//+genclient
+//+genclient:noStatus
+//+k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ClusterIssueOverride struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterIssueOverrideSpec   `json:"spec,omitempty"`
+	Status ClusterIssueOverrideStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ClusterIssueOverrideList contains a list of ClusterIssueOverride.
+type ClusterIssueOverrideList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterIssueOverride `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterIssueOverride{}, &ClusterIssueOverrideList{})
+}
