0bf1d48c71e0c65a24b804e2228da67fb1bdd28d
Matheus Moraes <matheusfaria.moraes@gmail.com>
2023-05-10 15:57:40 -0300

    init custom check API and controller

--- cmd/main.go
+++ cmd/main.go
@@ -148,6 +148,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ClusterScan")
 		os.Exit(1)
 	}
+	if err = (&zoracontroller.CustomCheckReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CustomCheck")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
