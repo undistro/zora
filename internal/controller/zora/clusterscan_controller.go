d611d527f8f9097c11fa5d3a8cbb9f1fca0f92ba
Matheus Moraes <matheusfaria.moraes@gmail.com>
2024-01-12 20:53:01 -0300

    chore: grant vulnerabilityreports/status permission to zora-operator

--- internal/controller/zora/clusterscan_controller.go
+++ internal/controller/zora/clusterscan_controller.go
@@ -74,7 +74,7 @@ type ClusterScanReconciler struct {
 //+kubebuilder:rbac:groups=zora.undistro.io,resources=clusterissues,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=zora.undistro.io,resources=clusterissues/status,verbs=get
 //+kubebuilder:rbac:groups=zora.undistro.io,resources=vulnerabilityreports,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=zora.undistro.io,resources=vulnerabilityreports/status,verbs=get
+//+kubebuilder:rbac:groups=zora.undistro.io,resources=vulnerabilityreports/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=batch,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch,resources=cronjobs/status,verbs=get
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch
