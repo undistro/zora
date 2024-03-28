da80f2f4b565ff5990d4b18b22370423dcd4db97
Matheus Moraes <matheusfaria.moraes@gmail.com>
2024-03-28 16:18:28 -0300

    unit test for NewScanStatus function

--- internal/saas/clusters_test.go
+++ internal/saas/clusters_test.go
@@ -16,11 +16,15 @@ package saas
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/yaml"
 
 	"github.com/undistro/zora/api/zora/v1alpha1"
@@ -122,4 +126,109 @@ func readClusterPayload(filename string) (Cluster, error) {
 		return cp, err
 	}
 	return cp, err
+}
+
+func TestNewScanStatus(t *testing.T) {
+	type args struct {
+		clusterScan *v1alpha1.ClusterScan
+		scans       []v1alpha1.ClusterScan
+	}
+	tests := []struct {
+		name            string
+		args            args
+		want            map[string]*PluginStatus
+		wantTotalIssues *int
+	}{
+		{
+			name: "current scan with 21 issues, previous with 19",
+			args: args{
+				clusterScan: &v1alpha1.ClusterScan{
+					ObjectMeta: metav1.ObjectMeta{Name: "mycluster-misconfig"},
+					Spec:       v1alpha1.ClusterScanSpec{Schedule: "17 * * * *"},
+					Status: v1alpha1.ClusterScanStatus{
+						TotalIssues:        pointer.Int(21),
+						LastScheduleTime:   mustParseTime("2024-03-28T14:17:00Z"),
+						LastFinishedTime:   mustParseTime("2024-03-28T14:17:27Z"),
+						LastSuccessfulTime: mustParseTime("2024-03-28T14:17:27Z"),
+						NextScheduleTime:   mustParseTime("2024-03-28T15:17:00Z"),
+						LastStatus:         "Complete",
+						LastFinishedStatus: "Complete",
+						Plugins: map[string]*v1alpha1.PluginScanStatus{
+							"marvin": {
+								LastScheduleTime:     mustParseTime("2024-03-28T14:17:00Z"),
+								LastFinishedTime:     mustParseTime("2024-03-28T14:17:27Z"),
+								LastSuccessfulTime:   mustParseTime("2024-03-28T14:17:27Z"),
+								NextScheduleTime:     mustParseTime("2024-03-28T15:17:00Z"),
+								LastScanID:           "9c6706f7-4d0e-4c79-bf70-efe1f4adc722",
+								LastSuccessfulScanID: "9c6706f7-4d0e-4c79-bf70-efe1f4adc722",
+								LastStatus:           "Complete",
+								LastFinishedStatus:   "Complete",
+								TotalIssues:          pointer.Int(21),
+							},
+						},
+					},
+				},
+				scans: []v1alpha1.ClusterScan{
+					{
+						ObjectMeta: metav1.ObjectMeta{Name: "mycluster-misconfig"},
+						Spec:       v1alpha1.ClusterScanSpec{Schedule: "17 * * * *"},
+						Status: v1alpha1.ClusterScanStatus{
+							TotalIssues:        pointer.Int(19),
+							LastScheduleTime:   mustParseTime("2024-03-28T14:17:00Z"),
+							LastFinishedTime:   nil,
+							LastSuccessfulTime: mustParseTime("2024-03-28T13:17:27Z"), // referring to the previous scan (1h before)
+							NextScheduleTime:   mustParseTime("2024-03-28T15:17:00Z"),
+							LastStatus:         "Active",
+							LastFinishedStatus: "Complete",
+							Plugins: map[string]*v1alpha1.PluginScanStatus{
+								"marvin": {
+									LastScheduleTime:     mustParseTime("2024-03-28T14:17:00Z"),
+									LastFinishedTime:     nil,
+									LastSuccessfulTime:   mustParseTime("2024-03-28T13:17:27Z"), // referring to the previous scan (1h before)
+									NextScheduleTime:     mustParseTime("2024-03-28T15:17:00Z"),
+									LastScanID:           "9c6706f7-4d0e-4c79-bf70-efe1f4adc722",
+									LastSuccessfulScanID: "3eadcc75-64c0-4498-a834-320c98a4da6c", // referring to the previous scan (1h before)
+									LastStatus:           "Active",
+									LastFinishedStatus:   "Complete",
+									TotalIssues:          pointer.Int(19),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: map[string]*PluginStatus{"marvin": {
+				Scan: &ScanStatus{
+					Status:  Scanned,
+					Suspend: false,
+					ID:      "9c6706f7-4d0e-4c79-bf70-efe1f4adc722",
+				},
+				IssueCount:             pointer.Int(21),
+				LastSuccessfulScanTime: mustParseTime("2024-03-28T14:17:27Z"),
+				LastFinishedScanTime:   mustParseTime("2024-03-28T14:17:27Z"),
+				NextScheduleScanTime:   mustParseTime("2024-03-28T15:17:00Z"),
+				Schedule:               "17 * * * *",
+				LastSuccessfulScanID:   "9c6706f7-4d0e-4c79-bf70-efe1f4adc722",
+			}},
+			wantTotalIssues: pointer.Int(21),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, totalIssues := NewScanStatus(tt.args.clusterScan, tt.args.scans)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewScanStatus() mismatch (-want +got):\n%s", cmp.Diff(tt.want, got))
+			}
+			if pointer.IntDeref(totalIssues, 0) == pointer.IntDeref(tt.wantTotalIssues, 0) {
+				t.Errorf("NewScanStatus() totalIssues = %v, want %v", totalIssues, tt.wantTotalIssues)
+			}
+		})
+	}
+}
+func mustParseTime(v string) *metav1.Time {
+	t, err := time.Parse(time.RFC3339, v)
+	if err != nil {
+		panic(fmt.Sprintf("mustParseTime(%s): %s", v, err.Error()))
+	}
+	return &metav1.Time{Time: t}
 }
