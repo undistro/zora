071bf3275966240fe8b301bd9021d40284acbd28
Matheus Moraes <matheusfaria.moraes@gmail.com>
2024-04-04 16:44:30 -0300

    chore: comment k8s client auth import

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,8 @@ import (
 	"strings"
 	"time"
 
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"go.uber.org/zap/zapcore"
